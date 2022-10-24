92c0733cde1f2426dcfda893c0948e04d0708e8e
Felipe <felipevera10@gmail.com>
2022-10-24 09:46:56 -0300

    incubadora

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* $(function() {
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hashtag = this.hash;
+
+      $("html, body").animate({
+        scrollTop: 
+      }, 500, function(){
+        window.location.hash = hashtag;
+      });
+    }
+  })
+}) */
